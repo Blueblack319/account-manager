4b83714b51378dce69acf5eb4fd9ebebb91eb86a
Yang Jaehoon <didwogns789@yonsei.ac.kr>
2022-09-06 10:05:58 +0900

    set up scripts and index.ts and .env

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 /**
  * Required External Modules
  */
+import * as dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+dotenv.config();
 
 /**
  * App Variables
  */
+const app = express();
 
 /**
  *  App Configuration
  */
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 /**
  * Server Activation
  */
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
