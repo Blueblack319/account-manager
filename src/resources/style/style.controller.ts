b87cef539b0131d001ef23e53813196124f8f8c5
Yang Jaehoon <didwogns789@yonsei.ac.kr>
2022-09-25 09:40:09 +0900

    test

--- src/resources/style/style.controller.ts
+++ src/resources/style/style.controller.ts
@@ -69,6 +69,7 @@ class StyleController implements Controller {
     try {
       const styles = await this.StyleService.findAll();
       res.status(200).json({ styles });
+      console.log('test');
     } catch (e) {
       if (e instanceof Error) {
         next(new HttpException(400, e.message));
